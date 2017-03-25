f60561780d06e73e4bf4cdd92b7e1ab87f7f537f
Atrix256 <alan.wolfe@gmail.com>
2017-03-25 07:55:13 -0700

    Begin

---
+++ Source.cpp
@@ -0,0 +1,34 @@
+
+int main (int argc, char **argv)
+{
+	return 0;
+}
+
+/*
+
+TODO:
+* This C++ utility takes a key framed vertex data texture in BMP format
+ * it takes params:
+ * it spits out a BMP and glsl and hlsl (?)
+* Watch this:  https://vimeo.com/200526114
+* get more sim data?
+ * particles following physical paths
+ * destructed object
+ * can we get a stable mesh fluid sim?
+? have a way to make inactive verts go straight to origin (some extra channel to multiply by maybe?)
+
+* WebGL Demo:
+ * make a demo that can compare the source, as well as other versions compressed with other parameters
+ 
+
+Blog Post:
+* explain how it works
+* explain how to use it
+* link to demo with screenshots
+* talk about how mesh has to be consistent from frame to frame
+* link to...
+ * http://www.gamasutra.com/blogs/TequilaWorks/20160620/275347/How_to_take_advantage_of_textures_in_the_vertex_shader.php
+ * http://advances.realtimerendering.com/other/2016/naughty_dog/
+ *  https://vimeo.com/200526114
+
+*/
